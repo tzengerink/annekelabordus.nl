eab8d591027e24e912c449f8755eadc85e993633
Teun Zengerink <t.zengerink@gmail.com>
2020-11-30 11:52:18 +0100

    Improved styling of navigation menu and items
    The active item is now underlined. Also clicking an already active menu
    item when in mobile view, ensures the menu is closed.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,19 @@
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { useState } from 'react'
 import CONFIG from '../config'
 import styles from './Navigation.module.css'
 
+const Item = ({ href, isActive, onClick, children }) => {
+    return (
+        <li className={`${styles.item} ${isActive ? styles.active : ''}`} onClick={onClick}>
+            <Link href={href}>{children}</Link>
+        </li>
+    )
+}
+
 const Navigation = () => {
+    const router = useRouter()
     const [isOpen, setIsOpen] = useState(false)
 
     return (
@@ -20,16 +30,21 @@ const Navigation = () => {
                 <h2>Werk</h2>
                 <ul>
                     {CONFIG.workTypes.map((type) => (
-                        <li key={type.name}>
-                            <Link href={`/${type.name}`}>{type.label}</Link>
-                        </li>
+                        <Item
+                            key={type.name}
+                            href={`/${encodeURIComponent(type.name)}`}
+                            isActive={router.asPath === `/${type.name}`}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            {type.label}
+                        </Item>
                     ))}
                 </ul>
                 <h2>Info</h2>
                 <ul>
-                    <li>
-                        <Link href="/over">Over</Link>
-                    </li>
+                    <Item href="/over" isActive={router.asPath === '/over'} onClick={() => setIsOpen(false)}>
+                        Over
+                    </Item>
                 </ul>
             </div>
         </div>
